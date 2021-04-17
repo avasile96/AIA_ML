b77e8456e91753ea9a7ad24c542ca2d701bf8329
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-04-18 01:51:27 +0200

    visible documentation
    + file reading improvements

--- source/main.py
+++ source/main.py
@@ -6,8 +6,9 @@ Created on Fri Apr 16 09:48:54 2021
 """
 
 import os
-from PIL import Image as PImage
 import gc
+import numpy as np
+from skimage import io
 
 
 class patient:
@@ -19,31 +20,28 @@ def loadImages(path):
     # return array of images from a directory (specified by "path")
     imagesList = os.listdir(path)
     loadedImages = []
-    
     for image in imagesList:
-        img = PImage.open(os.path.join(path, image))
+        img = io.imread(os.path.join(path, image))
         loadedImages.append(img)
-        
     return loadedImages
-
 
 def import_data(dataset_dir):
     # import ground truth and images (images are incorporated into the "patient" object)
     gt_dir = os.path.join(dataset_dir, 'groundtruth')
-    gt = loadImages(gt_dir)
-    
     patients_dir = os.path.join(dataset_dir, 'images')
-    patient_indexes = [ name for name in os.listdir(patients_dir) if os.path.isdir(os.path.join(patients_dir, name)) ]
     
     patients = []
-    for patient_index in patient_indexes:
-        images_dir = os.path.join(patients_dir, patient_index)
-        images = loadImages(images_dir)
-        p = patient(patient_index)
-        p.images = images
-        patients.append(p)
-    
-    return patients, gt
+    for patient_index in os.listdir(patients_dir):
+        if os.path.isdir(os.path.join(patients_dir, patient_index)):
+            gt = []
+            p = patient(patient_index)
+            p.images = loadImages(os.path.join(patients_dir, patient_index))
+            for name in os.listdir(gt_dir):
+                if patient_index in name:
+                    gt.append(io.imread(os.path.join(gt_dir, name)))
+            p.ground_truth = gt
+            patients.append(p)
+    return patients
 
 
 if __name__ == '__main__':
@@ -52,7 +50,7 @@ if __name__ == '__main__':
     project_dir = os.path.dirname(source_dir)
     dataset_dir = os.path.join(project_dir, 'dataset')
 
-    patients, gt = import_data(dataset_dir)
+    patients = import_data(dataset_dir)
     
     
     gc.collect()
