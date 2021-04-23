4f629d72268a78193890eab362d017ef78cd2981
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-04-23 10:08:54 +0200

    import model implementation

---
+++ source/main_import_model.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 09:56:55 2021
+
+@author: vasil
+"""
+
+import os
+import gc
+import numpy as np
+from skimage import io
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import load_img
+import keras
+from skimage.color import gray2rgb
+from skimage.transform import rescale, resize, downscale_local_mean
+
+tf.debugging.set_log_device_placement(True)
+
+
+img_size = (240, 320)
+num_classes = 256
+batch_size = 10
+
+source_dir = os.path.dirname(os.path.abspath(__name__))
+project_dir = os.path.dirname(source_dir)
+dataset_dir = os.path.join(project_dir, 'dataset')
+
+input_img_paths = []
+for patient_index in os.listdir(os.path.join(dataset_dir, 'images')):
+    if os.path.isdir(os.path.join(dataset_dir, 'images', patient_index)):
+        # patient_dir = os.path.join(dataset_dir, 'images', patient_index)
+        for fname in os.listdir(os.path.join(dataset_dir, 'images', patient_index)):
+            if fname.endswith(".bmp") and not fname.startswith("."):
+                input_img_paths.append(os.path.join(dataset_dir, 'images', patient_index, fname))
+
+target_img_paths = [
+        os.path.join(dataset_dir, 'groundtruth', fname)
+        for fname in os.listdir(os.path.join(dataset_dir, 'groundtruth'))
+        if fname.endswith(".tiff") and not fname.startswith(".")]
+
+class patient:
+    def __init__(self, index):
+        self.index = index
+
+def loadImages(path):
+    # return array of images from a directory (specified by "path")
+    imagesList = os.listdir(path)
+    loadedImages = []
+    for image in imagesList:
+        img = io.imread(os.path.join(path, image), as_gray = False)
+        loadedImages.append(img)
+    return loadedImages
+
+def downsample(img_arr, desired_dimesion = [224, 224]):
+    # Downsampling routine for stacks of images
+    # img arr = array of images to which you want to apply the downsampling
+    # desired_dimension = tuple of new dimension of the images
+    img_arr_ds = np.array([resize(image, desired_dimesion) for image in img_arr])
+    return img_arr_ds
+
+def create_patients(dataset_dir):
+    # create list of patients containing images from dataset
+    gt_dir = os.path.join(dataset_dir, 'groundtruth')
+    patients_dir = os.path.join(dataset_dir, 'images')
+    patients = []
+    for patient_index in os.listdir(patients_dir):
+        if os.path.isdir(os.path.join(patients_dir, patient_index)):
+            gt = []
+            p = patient(patient_index)
+            p.images = loadImages(os.path.join(patients_dir, patient_index))
+            for name in os.listdir(gt_dir):
+                if patient_index in name:
+                    gt.append(io.imread(os.path.join(gt_dir, name), as_gray = False))
+            p.ground_truth = gt
+            patients.append(p)
+    return patients
+
+def im_data_extract(list_of_patients):
+    # Extracting images from patients
+    # list_of_patients = list of patient objeccts
+    x_train = []
+    y_train = []
+    for patient in list_of_patients:
+        x_train.append(patient.images)
+        y_train.append(patient.ground_truth)
+        
+    x_train_arr = np.array([gray2rgb(image) for sublist in x_train for image in sublist])
+    y_train_arr = np.array([gray2rgb(image) for sublist in y_train for image in sublist])
+    
+    return x_train_arr, y_train_arr
+
+class IrisImageDatabase(keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.target_img_paths = target_img_paths
+
+    def __len__(self):
+        return len(self.target_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_target_img_paths = self.target_img_paths[i : i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size + (3,), dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = load_img(path, target_size=self.img_size)
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size + (1,), dtype="uint8")
+        for j, path in enumerate(batch_target_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = np.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
+            y[j] = tf.math.divide(y[j],255) - 1
+        return x, y
+
+def get_model(img_size, num_classes):
+    inputs = keras.Input(shape=img_size + (3,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+
+
+
+if __name__ == '__main__':
+    
+    # patients = create_patients(dataset_dir)
+    
+    #% Preparing training set
+    # from skimage.transform import rescale, resize, downscale_local_mean
+    # from skimage.color import gray2rgb
+    
+    # x_train_arr, y_train_arr = im_data_extract(patients)
+    
+    # x_train_arr_ds = downsample(x_train_arr)
+    # y_train_arr_ds = downsample(y_train_arr)
+    
+    #% VALIDATION SPLIT
+    import random
+
+    # Split our img paths into a training and a validation set
+    val_samples = int(len(target_img_paths)/2)
+    random.Random(1337).shuffle(input_img_paths)
+    random.Random(1337).shuffle(target_img_paths)
+    train_input_img_paths = input_img_paths[:-val_samples]
+    train_target_img_paths = target_img_paths[:-val_samples]
+    val_input_img_paths = input_img_paths[-val_samples:]
+    val_target_img_paths = target_img_paths[-val_samples:]
+    
+    # Instantiate data Sequences for each split
+    train_gen = IrisImageDatabase(batch_size, img_size, train_input_img_paths, train_target_img_paths)
+    val_gen = IrisImageDatabase(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+    
+    # U Net
+    # Configure the model for training.
+    # We use the "sparse" version of categorical_crossentropy
+    # because our target data is integers.
+    
+    # Free up RAM in case the model definition cells were run multiple times
+    keras.backend.clear_session()
+    
+    # Build model
+    model = get_model(img_size, num_classes)
+    model.summary()
+        
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics = ['accuracy'])
+    
+    import datetime
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    
+    from keras.models import load_model
+    model = load_model('oxford_segmentation.h5')
+    
+    keras.backend.clear_session()
+
+
