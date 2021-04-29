af64d54a35899efe0268c94ad86fa4576fe2ffc0
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-04-29 09:24:19 +0200

    up

--- source/im_proc.py
+++ source/im_proc.py
@@ -111,7 +111,7 @@ def k_means_seg(img):
     Z = np.float32(Z)
     # define criteria, number of clusters(K) and apply kmeans()
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 8
+    K = 3
     ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
     # Now convert back into uint8, and make original image
     center = np.uint8(center)
@@ -119,7 +119,7 @@ def k_means_seg(img):
     res2 = res.reshape((img.shape))    
     return res2
 
-
+#%%
 if __name__ == '__main__':
     
     patients = create_patients(dataset_dir)
@@ -166,6 +166,11 @@ if __name__ == '__main__':
 
     # Show in a window
     cv2.imshow('Contours', x_gray)
+    
+    #%% KMEANS
+    
+    im = k_means_seg(opening)
+    cv2.imshow('k_means_seg', im)
     
     #%% Iris Strip
     
