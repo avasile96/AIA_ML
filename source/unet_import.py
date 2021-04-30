a67845d3b82e9eae192f942609448677bfa2cf2a
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-04-30 11:41:24 +0200

    Update unet_import.py

--- source/unet_import.py
+++ source/unet_import.py
@@ -2,7 +2,10 @@
 """
 Created on Fri Apr 23 09:56:55 2021
 
-@author: vasil
+@author: alex
+
+This script imports the Unet and applies segmentation
+
 """
 
 import os
@@ -15,6 +18,7 @@ from tensorflow.keras.preprocessing.image import load_img
 import keras
 from skimage.color import gray2rgb
 from skimage.transform import rescale, resize, downscale_local_mean
+import random
 
 tf.debugging.set_log_device_placement(True)
 
@@ -39,57 +43,6 @@ target_img_paths = [
         os.path.join(dataset_dir, 'groundtruth', fname)
         for fname in os.listdir(os.path.join(dataset_dir, 'groundtruth'))
         if fname.endswith(".tiff") and not fname.startswith(".")]
-
-class patient:
-    def __init__(self, index):
-        self.index = index
-
-def loadImages(path):
-    # return array of images from a directory (specified by "path")
-    imagesList = os.listdir(path)
-    loadedImages = []
-    for image in imagesList:
-        img = io.imread(os.path.join(path, image), as_gray = False)
-        loadedImages.append(img)
-    return loadedImages
-
-def downsample(img_arr, desired_dimesion = [224, 224]):
-    # Downsampling routine for stacks of images
-    # img arr = array of images to which you want to apply the downsampling
-    # desired_dimension = tuple of new dimension of the images
-    img_arr_ds = np.array([resize(image, desired_dimesion) for image in img_arr])
-    return img_arr_ds
-
-def create_patients(dataset_dir):
-    # create list of patients containing images from dataset
-    gt_dir = os.path.join(dataset_dir, 'groundtruth')
-    patients_dir = os.path.join(dataset_dir, 'images')
-    patients = []
-    for patient_index in os.listdir(patients_dir):
-        if os.path.isdir(os.path.join(patients_dir, patient_index)):
-            gt = []
-            p = patient(patient_index)
-            p.images = loadImages(os.path.join(patients_dir, patient_index))
-            for name in os.listdir(gt_dir):
-                if patient_index in name:
-                    gt.append(io.imread(os.path.join(gt_dir, name), as_gray = False))
-            p.ground_truth = gt
-            patients.append(p)
-    return patients
-
-def im_data_extract(list_of_patients):
-    # Extracting images from patients
-    # list_of_patients = list of patient objeccts
-    x_train = []
-    y_train = []
-    for patient in list_of_patients:
-        x_train.append(patient.images)
-        y_train.append(patient.ground_truth)
-        
-    x_train_arr = np.array([gray2rgb(image) for sublist in x_train for image in sublist])
-    y_train_arr = np.array([gray2rgb(image) for sublist in y_train for image in sublist])
-    
-    return x_train_arr, y_train_arr
 
 class IrisImageDatabase(keras.utils.Sequence):
     """Helper to iterate over the data (as Numpy arrays)."""
@@ -123,6 +76,19 @@ class IrisImageDatabase(keras.utils.Sequence):
             # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
             y[j] = tf.math.divide(y[j],255)
         return x, y
+
+def GetTestTrainGenerators(val_percent):
+    val_samples = int(len(target_img_paths)*val_percent/100)
+    random.Random(1337).shuffle(input_img_paths)
+    random.Random(1337).shuffle(target_img_paths)
+    train_input_img_paths = input_img_paths[:-val_samples]
+    train_target_img_paths = target_img_paths[:-val_samples]
+    val_input_img_paths = input_img_paths[-val_samples:]
+    val_target_img_paths = target_img_paths[-val_samples:]
+    # Instantiate data Sequences for each split
+    train_gen = IrisImageDatabase(batch_size, img_size, train_input_img_paths, train_target_img_paths)
+    val_gen = IrisImageDatabase(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+    return train_gen, val_gen
 
 def get_model(img_size, num_classes):
     #Build the model
@@ -188,45 +154,31 @@ def get_model(img_size, num_classes):
     return model
 
 
-
 if __name__ == '__main__':
     
-    #% VALIDATION SPLIT
-    import random
+    # Train-Validation Split
+    train_gen, val_gen = GetTestTrainGenerators(1)
 
-    # Split our img paths into a training and a validation set
-    val_samples = int(len(target_img_paths)/100)
-    random.Random(1337).shuffle(input_img_paths)
-    random.Random(1337).shuffle(target_img_paths)
-    train_input_img_paths = input_img_paths[:-val_samples]
-    train_target_img_paths = target_img_paths[:-val_samples]
-    val_input_img_paths = input_img_paths[-val_samples:]
-    val_target_img_paths = target_img_paths[-val_samples:]
-    
-    # Instantiate data Sequences for each split
-    train_gen = IrisImageDatabase(batch_size, img_size, train_input_img_paths, train_target_img_paths)
-    val_gen = IrisImageDatabase(batch_size, img_size, val_input_img_paths, val_target_img_paths)
-    
-    # Import U-Net
-    
+    #%% SEGMENTATION
     # Free up RAM in case the model definition cells were run multiple times
     keras.backend.clear_session()
-    
-    # Build model
-    
-    import datetime
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-    
+    # Import U-Net
     from keras.models import load_model
     model = load_model('iris_unet.h5')
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics = ['accuracy'])
-    
+    # Get predictions (segment) images from the dataset
+    # "a" contains images segmented by UNet
     a = model.predict(
-    val_gen, batch_size=2, verbose=2, steps=None, callbacks=None, max_queue_size=10,
-    workers=2, use_multiprocessing=False)
+        val_gen, 
+        batch_size=2, 
+        verbose=2, 
+        steps=None, 
+        callbacks=None, 
+        max_queue_size=10,
+        workers=2, 
+        use_multiprocessing=False)
     
+    #%% POLAR TRANSFORM
 
 
 
