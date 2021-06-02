59b568e3aa5a5874fd719fb47caa74cd3fbb5eb2
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-06-02 12:35:11 +0200

    up

--- source/classification_ae_features.py
+++ source/classification_ae_features.py
@@ -27,99 +27,54 @@ tf.debugging.set_log_device_placement(True)
 
 img_size = (240, 320)
 num_classes = 224
-batch_size = 1
+batch_size = 64
 
 source_dir = os.path.dirname(os.path.abspath(__name__))
 project_dir = os.path.dirname(source_dir)
 big_file_dir = os.path.dirname(project_dir)
 dataset_dir = os.path.join(project_dir, 'dataset')
 strip_folder = os.path.join(source_dir, 'strips')
-# db = pd.read_csv(big_file_dir+"\\features.csv")
-db_path = big_file_dir+"\\deep_features.csv"
+db_path = project_dir+"\\deep_features.csv"
 
-# # Getting labels and features
-# labels = db_np[:,0]
-# classes = np.unique(labels)
-# nr_of_classes = classes.shape[0]
-# features = db_np[:,1:]
-
-
-class FeaturesLabelsData(keras.utils.Sequence):
-    """Helper to iterate over the data (as Numpy arrays)."""
-
-    def __init__(self, batch_size, db_path, mode = 'train'):
-        self.batch_size = batch_size
-        self.db_path = db_path
-        self.file = open(db_path,'r')
-        self.reader = csv.reader(self.file)
-        self.mode = mode
-        
-    def __len__(self):
-        return len(list(self.reader)) // self.batch_size
-
-    def __getitem__(self, idx):
-        """Returns tuple (input, target) correspond to batch #idx."""
-        while True:
-            # initialize our batch of data and labels
-            data = []
-            labels = []
-            numClasses = 225
-            # keep looping until we reach our batch size
-            while len(data) < self.batch_size:
-                # attempt to read the next row of the CSV file
-                row = self.file.readline()
-                # check to see if the row is empty, indicating we have
-                # reached the end of the file
-                if row == "":
-                    # reset the file pointer to the beginning of the file
-                    # and re-read the row
-                    self.file.seek(0)
-                    row = self.file.readline()
-                    # if we are evaluating we should now break from our
-                    # loop to ensure we don't continue to fill up the
-                    # batch from samples at the beginning of the file
-                    if self.mode == "eval":
-                        break
-                # extract the class label and features from the row
-                row = row.strip().split(",")
-                label = row[0]
-                # label = to_categorical(label, num_classes=numClasses)[np.newaxis].T
-                features = np.array(row[1:], dtype="float")
-                # update the data and label lists
-                data.append(features)
-                labels.append(label)
-                
-                # yield the batch to the calling function
-            return (np.squeeze(np.array(data)).reshape(-1, 100352), np.squeeze(np.array(labels)).reshape(-1, 225))
 
 if __name__ == '__main__':
     
-    # Reading from csv
-    # db = pd.read_csv(big_file_dir+"\\features.csv")
-    # db_np = db.to_numpy()
+    #%% Data Reading
+    train = pd.read_csv(db_path, index_col=(0))
+    train_x = np.array(train)
+    train_y = to_categorical(np.array(train.index), 225)
     
-    # Getting labels and features
-    # labels = db_np[:,0]
-    # classes = np.unique(labels)
-    # nr_of_classes = classes.shape[0]
-    # features = db_np[:,1:]
+    #%% 50-50 Split
+    x_train = []
+    y_train = []
+   
+    x_val = [train_x[0]]
+    y_val = [train_y[0]]
     
-    # # Scaling
-    # features_scaled = MinMaxScaler().fit_transform(features)
+    for i in range(0,2250,10):
+        print(i)
+        x_train.extend(train_x[i:i+5])
+        y_train.extend(train_y[i:i+5])
+        
+        x_val.extend(train_x[i+6:i+11])
+        y_val.extend(train_y[i+6:i+11])
+        
+    x_train = np.array(x_train, dtype = np.float)
+    x_val = np.array(x_val, dtype = np.float)
     
-    #%% Abit of data exploration
-    # nonzero_element_count = np.count_nonzero(features_scaled)
-    # zero_element_count = features_scaled.size - np.count_nonzero(features_scaled)
+    y_train = np.array(y_train, dtype = np.int)
+    y_val = np.array(y_val, dtype = np.int)
     
-    # Generator from csv
-    # train_gen = FeaturesLabelsData(batch_size, db_path, mode = 'train')
-    train = pd.read_csv(db_path, header = None)
-    train_x
-    train_y
-    # del db, db_np, features
+    plt.figure()
+    io.imshow(y_train)
+    plt.title('y_train')
     
-    #%% Model
-    # define our simple neural network
+    plt.figure()
+    io.imshow(y_val)
+    plt.title('y_val')
+    del train, train_x, train_y
+    
+    #%% Shallow neural net
     """
     How did I come up with the values of 256 and 16 for the two hidden layers?
     A good rule of thumb is to take the square root of the previous number of nodes 
@@ -128,27 +83,29 @@ if __name__ == '__main__':
     is then 16, thus giving us our architecture definition.
     """
     model = Sequential()
-    model.add(Dense(512, input_shape=(7 * 7 * 2048,), activation="relu"))
+    model.add(Dense(512, input_shape=(9600,), activation="relu"))
     model.add(tf.keras.layers.Dropout(0.2))
-    model.add(Dense(256, activation="relu"))
-    model.add(tf.keras.layers.Dropout(0.2))
-    model.add(Dense(225))
-    # model = tf.keras.models.Sequential([
-    #           tf.keras.layers.Dense(input_shape=(7 * 7 * 2048,)),
-    #           tf.keras.layers.Dense(512, activation='relu'),
-    #           tf.keras.layers.Dropout(0.2),
-    #           tf.keras.layers.Dense(num_classes)
-    #           ])
-    # compile the model
-    # loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+    model.add(Dense(225, activation="softmax"))
     loss_fn = tf.keras.losses.CategoricalCrossentropy()
 
-    model.compile(loss=loss_fn, optimizer='adam',
-    	metrics=["accuracy"])
+    model.compile(loss=loss_fn, optimizer='adam', metrics=["accuracy"])
+    n_ep = 10
     # Train 
-    history = model.fit(train_gen, epochs=20, verbose = 2, batch_size = batch_size)
-    # Evaluate
-    # model.evaluate(features_scaled,  labels, verbose=2)    
-
+    history = model.fit(x=x_train, y=y_train, validation_data = (x_val, y_val), epochs=n_ep, verbose = 2, batch_size = batch_size)
+    
+    #%% Plotting
+    y_ax = np.linspace(0,100,len(history.history["accuracy"]), dtype = np.int)
+    x_ax = np.linspace(0,n_ep,len(history.history["accuracy"]), dtype = np.int)
+    
+    plt.figure()
+    plt.plot(x_ax,np.array(history.history["loss"]))
+    plt.xlabel('epochs')
+    plt.title('Shallow Net Classification Loss')
+    
+    plt.figure()
+    plt.plot(x_ax,np.array(history.history["accuracy"])*100)
+    plt.xlabel('epochs')
+    plt.ylabel('[%]')
+    plt.title('Shallow Net Classification Accuracy')
     
     
